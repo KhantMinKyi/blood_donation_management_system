0749beafb55df7c3dc59140250cce0cb8b5eb651
Zarni Tun <zarnitun1233@gmail.com>
2023-07-02 23:13:30 +0630

    edited homepage's search feature

--- resources/views/homepage.blade.php
+++ resources/views/homepage.blade.php
@@ -20,148 +20,45 @@
                 <div class="form-group text-center justify-content-center col-sm-12 col-lg-4 col-md-6">
 
                     <select style="width: 220px; height: 45px;" name="city" id="city" class="form-control demo-default mx-auto" required>
-                        <option value="">-- Select --</option>
-                        <optgroup title="Azad Jammu and Kashmir (Azad Kashmir)" label="&raquo; Azad Jammu and Kashmir (Azad Kashmir)"></optgroup>
-                        <option value="Bagh">Bagh</option>
-                        <option value="Bhimber">Bhimber</option>
-                        <option value="Kotli">Kotli</option>
-                        <option value="Mirpur">Mirpur</option>
-                        <option value="Muzaffarabad">Muzaffarabad</option>
-                        <option value="Neelum">Neelum</option>
-                        <option value="Poonch">Poonch</option>
-                        <option value="Sudhnati">Sudhnati</option>
-                        <optgroup title="Balochistan" label="&raquo; Balochistan"></optgroup>
-                        <option value="Awaran">Awaran</option>
-                        <option value="Barkhan">Barkhan</option>
-                        <option value="Bolan">Bolan</option>
-                        <option value="Chagai">Chagai</option>
-                        <option value="Dera Bugti">Dera Bugti</option>
-                        <option value="Gwadar">Gwadar</option>
-                        <option value="Jafarabad">Jafarabad</option>
-                        <option value="Jhal Magsi">Jhal Magsi</option>
-                        <option value="Kalat">Kalat</option>
-                        <option value="Kech">Kech</option>
-                        <option value="Kharan">Kharan</option>
-                        <option value="Khuzdar">Khuzdar</option>
-                        <option value="Kohlu">Kohlu</option>
-                        <option value="Lasbela">Lasbela</option>
-                        <option value="Loralai">Loralai</option>
-                        <option value="Mastung">Mastung</option>
-                        <option value="Musakhel">Musakhel</option>
-                        <option value="Naseerabad">Naseerabad</option>
-                        <option value="Nushki">Nushki</option>
-                        <option value="Panjgur">Panjgur</option>
-                        <option value="Pishin">Pishin</option>
-                        <option value="Qilla Abdullah">Qilla Abdullah</option>
-                        <option value="Qilla Saifullah">Qilla Saifullah</option>
-                        <option value="Quetta">Quetta</option>
-                        <option value="Sibi">Sibi</option>
-                        <option value="Zhob">Zhob</option>
-                        <option value="Ziarat">Ziarat</option>
-                        <optgroup title="Federally Administered Tribal Areas (FATA" label="&raquo; Federally Administered Tribal Areas (FATA"></optgroup>
-                        <option value="Bajaur Agency">Bajaur Agency</option>
-                        <option value="Khyber Agency">Khyber Agency</option>
-                        <option value="Kurram Agency">Kurram Agency</option>
-                        <option value="Mohmand Agency">Mohmand Agency</option>
-                        <option value="North Waziristan Agency">North Waziristan Agency</option>
-                        <option value="Orakzai Agency">Orakzai Agency</option>
-                        <option value="South Waziristan Agency">South Waziristan Agency</option>
-                        <optgroup title="Islamabad Capital" label="&raquo; Islamabad Capital"></optgroup>
-                        <option value="Islamabad">Islamabad</option>
-                        <optgroup title="North-West Frontier Province (NWFP)" label="&raquo; North-West Frontier Province (NWFP)"></optgroup>
-                        <option value="Abbottabad">Abbottabad</option>
-                        <option value="Bannu">Bannu</option>
-                        <option value="Batagram">Batagram</option>
-                        <option value="Buner">Buner</option>
-                        <option value="Charsadda">Charsadda</option>
-                        <option value="Chitral">Chitral</option>
-                        <option value="Dera Ismail Khan">Dera Ismail Khan</option>
-                        <option value="Dir Lower">Dir Lower</option>
-                        <option value="Dir Upper">Dir Upper</option>
-                        <option value="Hangu">Hangu</option>
-                        <option value="Haripur">Haripur</option>
-                        <option value="Karak">Karak</option>
-                        <option value="Kohat">Kohat</option>
-                        <option value="Kohistan">Kohistan</option>
-                        <option value="Lakki Marwat">Lakki Marwat</option>
-                        <option value="Malakand">Malakand</option>
-                        <option value="Mansehra">Mansehra</option>
-                        <option value="Mardan">Mardan</option>
-                        <option value="Nowshera">Nowshera</option>
-                        <option value="Peshawar">Peshawar</option>
-                        <option value="Shangla">Shangla</option>
-                        <option value="Swabi">Swabi</option>
-                        <option value="Swat">Swat</option>
-                        <option value="Tank">Tank</option>
-                        <optgroup title="Punjab" label="&raquo; Punjab"></optgroup>
-                        <option value="Alipur">Alipur</option>
-                        <option value="Attock">Attock</option>
-                        <option value="Bahawalnagar">Bahawalnagar</option>
-                        <option value="Bahawalpur">Bahawalpur</option>
-                        <option value="Bhakkar">Bhakkar</option>
-                        <option value="Chakwal">Chakwal</option>
-                        <option value="Chiniot">Chiniot</option>
-                        <option value="Dera Ghazi Khan">Dera Ghazi Khan</option>
-                        <option value="Faisalabad">Faisalabad</option>
-                        <option value="Gujranwala">Gujranwala</option>
-                        <option value="Gujrat">Gujrat</option>
-                        <option value="Hafizabad">Hafizabad</option>
-                        <option value="Jhang">Jhang</option>
-                        <option value="Jhelum">Jhelum</option>
-                        <option value="Kasur">Kasur</option>
-                        <option value="Khanewal">Khanewal</option>
-                        <option value="Khushab">Khushab</option>
-                        <option value="Lahore">Lahore</option>
-                        <option value="Layyah">Layyah</option>
-                        <option value="Lodhran">Lodhran</option>
-                        <option value="Mandi Bahauddin">Mandi Bahauddin</option>
-                        <option value="Mianwali">Mianwali</option>
-                        <option value="Multan">Multan</option>
-                        <option value="Muzaffargarh">Muzaffargarh</option>
-                        <option value="Nankana Sahib">Nankana Sahib</option>
-                        <option value="Narowal">Narowal</option>
-                        <option value="Okara">Okara</option>
-                        <option value="Pakpattan">Pakpattan</option>
-                        <option value="Rahim Yar Khan">Rahim Yar Khan</option>
-                        <option value="Rajanpur">Rajanpur</option>
-                        <option value="Rawalpindi">Rawalpindi</option>
-                        <option value="Sahiwal">Sahiwal</option>
-                        <option value="Sargodha">Sargodha</option>
-                        <option value="Sheikhupura">Sheikhupura</option>
-                        <option value="Shekhupura">Shekhupura</option>
-                        <option value="Sialkot">Sialkot</option>
-                        <option value="Toba Tek Singh">Toba Tek Singh</option>
-                        <option value="Vehari">Vehari</option>
-                        <optgroup title="Sindh" label="&raquo; Sindh"></optgroup>
-                        <option value="Badin">Badin</option>
-                        <option value="Dadu">Dadu</option>
-                        <option value="Ghotki">Ghotki</option>
-                        <option value="Hyderabad">Hyderabad</option>
-                        <option value="Jacobabad">Jacobabad</option>
-                        <option value="Jamshoro">Jamshoro</option>
-                        <option value="Karachi">Karachi</option>
-                        <option value="Kashmore">Kashmore</option>
-                        <option value="Khairpur">Khairpur</option>
-                        <option value="Larkana">Larkana</option>
-                        <option value="Matiari">Matiari</option>
-                        <option value="Mirpur Khas">Mirpur Khas</option>
-                        <option value="Naushahro Feroze">Naushahro Feroze</option>
-                        <option value="Nawabshah">Nawabshah</option>
-                        <option value="Qambar Shahdadkot">Qambar Shahdadkot</option>
-                        <option value="Sanghar">Sanghar</option>
-                        <option value="Shikarpur">Shikarpur</option>
-                        <option value="Sukkur">Sukkur</option>
-                        <option value="Tando Allahyar">Tando Allahyar</option>
-                        <option value="Tando Muhammad Khan">Tando Muhammad Khan</option>
-                        <option value="Tharparkar">Tharparkar</option>
-                        <option value="Thatta">Thatta</option>
-                        <option value="Umerkot">Umerkot</option>
+                        <option value="">Search By Township</option>
+                        <optgroup title="Western Yangon" label="&raquo; Western Yangon"></optgroup>
+                        <option value="Kamaryut">Kamaryut</option>
+                        <option value="Kyauktada">Kyauktada</option>
+                        <option value="Kyimyindine">Kyimyindine</option>
+                        <option value="Sangyoung">Sangyoung</option>
+                        <option value="Seikkan">Seikkan</option>
+                        <option value="Pabedann">Pabedann</option>
+                        <option value="Bahann">Bahann</option>
+                        <option value="Lanmadaw">Lanmadaw</option>
+                        <optgroup title="Southern Yangon" label="&raquo; Southern Yangon"></optgroup>
+                        <option value="Kawhmu">Kawhmu</option>
+                        <option value="Kyauktan">Kyauktan</option>
+                        <option value="Kungyangonn">Kungyangonn</option>
+                        <option value="Kayan">Kayan</option>
+                        <option value="Dalla">Dalla</option>
+                        <option value="Thongwa">Thongwa</option>
+                        <option value="Thanlyin">Thanlyin</option>
+                        <optgroup title="Northern Yangon" label="&raquo; Northern Yangon"></optgroup>
+                        <option value="Taikkyi">Taikkyi</option>
+                        <option value="Htantabin">Htantabin</option>
+                        <option value="Shwepyitha">Shwepyitha</option>
+                        <option value="Hlinethaya">Hlinethaya</option>
+                        <option value="Hlegu">Hlegu</option>
+                        <option value="Mingalataungnyunt">Mingalataungnyunt</option>
+                        <option value="Insein">Insein</option>
+                        <optgroup title="Eastern Yangon" label="&raquo; Eastern Yangon"></optgroup>
+                        <option value="Tarmwe">Tarmwe</option>
+                        <option value="South Okkalapa">South Okkalapa</option>
+                        <option value="Dawbon">Dawbon</option>
+                        <option value="Pazundaung">Pazundaung</option>
+                        <option value="Botahtaung">Botahtaung</option>
+                        <option value="Mingalataungnyunt">Mingalataungnyunt</option>
+                        <option value="Yankin">Yankin</option>
                     </select>
                 </div>
                 <div class="form-group center-aligned col-sm-12 col-lg-4 col-md-6">
                     <select name="blood_group" id="blood_group" style="padding: 0 20px; width: 220px; height: 45px;" class="form-control demo-default text-center margin10px mx-auto">
-
-                        <option value="A+">A+</option>
+                    <option value="">Search By Blood Type</option>
                         <option value="A-">A-</option>
                         <option value="B+">B+</option>
                         <option value="B-">B-</option>
